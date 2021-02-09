bb4f85ea63dd61e4a6fa4bbba74c97dc56826e33
Rafael Luz <rafaellladm@gmail.com>
2021-02-08 23:00:07 -0300

    criaçao de server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static("public"))
+
+server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", (req, res) => {
+    return res.render("index")
+})
+
+server.listen(5000, () => {
+    console.log('server is running')
+})
