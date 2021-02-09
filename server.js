85d96e3375668df8a61d46c4eb2a706caf93c425
Rafael Luz <rafaellladm@gmail.com>
2021-02-09 01:17:22 -0300

    configuraçao nunjucks e criaçao de caminhos

--- server.js
+++ server.js
@@ -5,15 +5,26 @@ const server = express()
 
 server.use(express.static("public"))
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    // autoescape: false,
+    noCache: true
 })
 
 server.get("/", (req, res) => {
-    return res.render("index")
+    return res.render("about")
 })
+
+server.get("/receitas", (req, res) => {
+    return res.render("receitas")
+})
+
+server.get("/sobre", (req, res) => {
+    return res.render("sobre")
+})
+
 
 server.listen(5000, () => {
     console.log('server is running')
