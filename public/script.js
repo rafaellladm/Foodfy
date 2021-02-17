33abe61af4b1c3aa21a065bc7f1e15d3cc90df1c
Rafael Luz <rafaellladm@gmail.com>
2021-02-17 18:01:30 -0300

    criaçao de esconder e mostra list

--- public/script.js
+++ public/script.js
@@ -1,12 +1,26 @@
-// Identifica modal
-const modalOverlay = document.querySelector('.modal');
-
-// ativar modal
+// enviar informações para nova pagina
 const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener('click', function(){
         const cardId = card.getAttribute('id')
         window.location.href = `/receitas/${cardId}`
+        console.log(card)
+    })
+}
+
+const buttons = document.querySelectorAll('.hide_show')
+
+for (let button of buttons) {
+    button.addEventListener('click', () => {
+        const hide = button.getAttribute('id')
+
+        if (document.querySelector(`.list_${hide}`).style.display == 'none') {
+            document.querySelector(`.list_${hide}`).style.display = 'block'
+            button.innerHTML = "ESCONDER"
+        }else {
+            document.querySelector(`.list_${hide}`).style.display = 'none'
+            button.innerHTML = "MOSTRAR"
+        }
     })
 }
